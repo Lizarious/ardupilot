f7a49c5bf78b92c02096633c7c15eda84055c917
Lizarious <samrkronk@gmail.com>
2026-03-05 18:31:45 -0800

    frames done i think

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -175,9 +175,24 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
         add_motor_raw_6dof(AP_MOTORS_MOT_6,     -1.0f,          0,              0,              -1.0f,              0,                  0,              6);
         break;
 
-    case SUB_FRAME_CUSTOM:
-        // Put your custom motor setup here
-        //break;
+        //                 Motor #              Roll Factor     Pitch Factor    Yaw Factor      Throttle Factor     Forward Factor      Lateral Factor  Testing Order
+    case SUB_FRAME_PEENAR:
+        _frame_class_string = "PEENAR";
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,     0,              0,              0,              0,                  1.0f,               0,              1);
+        add_motor_raw_6dof(AP_MOTORS_MOT_2,     0,              0,              -1.0f,          0,                  -1.0f,              -1.0f,          2);
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,     0,              0,              -1.0f,          0,                  1.0f,               1.0f,           3);
+        add_motor_raw_6dof(AP_MOTORS_MOT_4,     0,              0,              1.0f,           0,                  1.0f,               -1.0f,          4);
+        break;
+
+    case SUB_FRAME_WINSLOW:
+        _frame_class_string = "WINSLOW";
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,     0,              0,              1.0f,           0,                  1.0f,               0,              1);
+        add_motor_raw_6dof(AP_MOTORS_MOT_2,     0,              0,              -1.0f,          0,                  1.0f,               0,              2);
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,     1.0f,           -1.0f,          0,              0.45f,                  0,                  -1.0f,          3);
+        add_motor_raw_6dof(AP_MOTORS_MOT_4,     -1.0f,          -1.0f,          0,              0.45f,                  0,                  1.0f,           4);
+        add_motor_raw_6dof(AP_MOTORS_MOT_5,     -1.0f,          -1.0f,          0,              0.45f,              0,                  -1.0f,          5);
+        add_motor_raw_6dof(AP_MOTORS_MOT_6,     1.0f,           -1.0f,          0,              0.45f,              0,                  1.0f,           6);
+    break;
 
     case SUB_FRAME_SIMPLEROV_3:
         _frame_class_string = "SIMPLEROV_3";
